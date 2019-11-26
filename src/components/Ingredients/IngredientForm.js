49cef4eab544b6e96736177b0ed88847b03ef217
Murphy Hsieh <murphyhsieh@leandev.io>
2019-11-26 17:21:07 +0800

    加強整個更新 input value 的過程，包含避免 event object 的重複使用

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -19,12 +19,28 @@ const IngredientForm = React.memo(props => {
                     <div className="form-control">
                         <label htmlFor="title">Name</label>
                         <input type="text" id="title" vlaue={inputState[0].title}
-                               onChange={event => inputState[1]({title: event.target.value})}/>
+                               onChange={event => {
+                                   const newTitle = event.target.value;
+                                   inputState[1](prevInputState => ({
+                                           title: newTitle,
+                                           amount: prevInputState.amount
+                                       })
+                                   )
+                               }}
+                        />
                     </div>
                     <div className="form-control">
                         <label htmlFor="amount">Amount</label>
                         <input type="number" id="amount" value={inputState[0].amount}
-                               onChange={event => inputState[1]({amount: event.target.value})}/>
+                               onChange={event => {
+                                   const newAmount = event.target.value;
+                                   inputState[1](prevInputState => ({
+                                           amount: newAmount,
+                                           title: prevInputState.title
+                                       })
+                                   )
+                               }}
+                        />
                     </div>
                     <div className="ingredient-form__actions">
                         <button type="submit">Add Ingredient</button>
