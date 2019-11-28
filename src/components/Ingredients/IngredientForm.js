83c9eff363f4db3b6d8908b59e4e0ed7f0dbb0a4
Murphy Hsieh <murphyhsieh@leandev.io>
2019-11-28 16:44:47 +0800

    在 Ingredient From 上層管理全部 ingredients、利用realtime-database 發送簡單 http request

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -10,7 +10,10 @@ const IngredientForm = React.memo(props => {
 
     const submitHandler = event => {
         event.preventDefault();
-        // ...
+        props.onAddIngredient({
+            title: enteredTitle,
+            amount: enteredAmount
+        });
     };
 
     return (
--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,19 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import IngredientForm from './IngredientForm';
+import IngredientList from "./IngredientList";
 import Search from './Search';
 
 function Ingredients() {
-  return (
-    <div className="App">
-      <IngredientForm />
 
-      <section>
-        <Search />
-        {/* Need to add list here! */}
-      </section>
-    </div>
-  );
+    const [ingredients, setIngredients] = useState([]);
+
+    const  addIngredient = async (ingredient) => {
+
+        // talk to server
+        const response = await fetch('https://react-course-hooks.firebaseio.com/ingredients.json', {
+            method: 'POST',
+            body: JSON.stringify(ingredient),
+            headers: {'Content-Type': 'application.json'}
+        });
+        const responseData = await response.json();
+
+        setIngredients(prevIngredients => [
+            ...prevIngredients,
+            {
+                id: responseData.name,
+                ...ingredient // contain title and amount
+            }]
+        );
+    };
+
+    return (
+        <div className="App">
+            <IngredientForm onAddIngredient={addIngredient}/>
+
+            <section>
+                <Search/>
+                <IngredientList ingredients={ingredients} onRemoveItem={() => {
+                }}/>
+            </section>
+        </div>
+    );
 }
 
 export default Ingredients;
