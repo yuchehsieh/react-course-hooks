dbca71088174f273980fa09ef58d594333441ce4
Murphy Hsieh <murphyhsieh@leandev.io>
2019-11-28 17:39:54 +0800

    使用最簡單的 useEffect

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from "./IngredientList";
@@ -8,7 +8,29 @@ function Ingredients() {
 
     const [ingredients, setIngredients] = useState([]);
 
-    const  addIngredient = async (ingredient) => {
+    useEffect(() => {
+
+        async function fetchData() {
+            const response = await fetch('https://react-course-hooks.firebaseio.com/ingredients.json');
+            const responseData = await response.json();
+            let loadedData = [];
+            for (let key in responseData) {
+                loadedData.push({
+                    id: key,
+                    ...responseData[key],
+                })
+            }
+
+            console.log(loadedData);
+            setIngredients(loadedData);
+        }
+
+        fetchData();
+
+    }, []);
+
+
+    const addIngredient = async (ingredient) => {
 
         // talk to server
         const response = await fetch('https://react-course-hooks.firebaseio.com/ingredients.json', {
