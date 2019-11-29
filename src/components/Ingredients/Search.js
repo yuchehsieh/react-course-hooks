99870cfa04bf95eec5bdb0297446eb439e74248a
Murphy Hsieh <murphyhsieh@leandev.io>
2019-11-29 09:29:58 +0800

    使用 setTimeout 配合 useRef() 去減少跟伺服器要資料的次數

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
@@ -7,32 +7,43 @@ const Search = React.memo(props => {
 
     const {onLoadingIngredient} = props;
     const [enteredFilter, setEnteredFilter] = useState('');
+    const inputRef = useRef();
 
     useEffect(() => {
-        async function fetchFilteredData() {
-            const query = enteredFilter.length === 0 ? '' : `?orderBy="title"&equalTo="${enteredFilter}"`;
-            const response = await fetch('https://react-course-hooks.firebaseio.com/ingredients.json' + query);
-            const responseData = await response.json();
-            let loadedData = [];
-            for (let key in responseData) {
-                loadedData.push({
-                    id: key,
-                    ...responseData[key],
-                })
+
+        setTimeout(() => {
+
+            if(enteredFilter === inputRef.current.value) {
+
+                async function fetchFilteredData() {
+                    const query = enteredFilter.length === 0 ? '' : `?orderBy="title"&equalTo="${enteredFilter}"`;
+                    const response = await fetch('https://react-course-hooks.firebaseio.com/ingredients.json' + query);
+                    const responseData = await response.json();
+                    let loadedData = [];
+                    for (let key in responseData) {
+                        loadedData.push({
+                            id: key,
+                            ...responseData[key],
+                        })
+                    }
+
+                    onLoadingIngredient(loadedData);
+                }
+
+                fetchFilteredData();
             }
 
-            onLoadingIngredient(loadedData);
-        }
+        } , 500)
 
-        fetchFilteredData();
-    }, [enteredFilter, onLoadingIngredient]);
+
+    }, [enteredFilter, onLoadingIngredient, inputRef]);
 
     return (
         <section className="search">
             <Card>
                 <div className="search-input">
                     <label>Filter by Title</label>
-                    <input type="text" value={enteredFilter}
+                    <input type="text" value={enteredFilter} ref={inputRef}
                            onChange={e => setEnteredFilter(e.target.value)}/>
                 </div>
             </Card>
