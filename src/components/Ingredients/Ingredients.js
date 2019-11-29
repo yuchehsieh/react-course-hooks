3553a5a58937f4832452bea22cacc2b8194538f4
Murphy Hsieh <murphyhsieh@leandev.io>
2019-11-29 12:14:08 +0800

    製作簡單的 useReducer hooks. Whenever working with useReducer, React will re-render the component whever your reducer return the bew value

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,13 +1,27 @@
-import React, {useState, useEffect, useCallback} from 'react';
+import React, {useState, useEffect, useCallback, useReducer} from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from "./IngredientList";
 import ErrorModal from "../UI/ErrorModal";
 import Search from './Search';
 
+const ingredientsReducer = (currentIngredients, action) => {
+    switch (action.type) {
+        case 'SET':
+            return action.ingredients;
+        case 'ADD':
+            return [...currentIngredients, action.ingredient];
+        case 'DELETE':
+            return currentIngredients.filter(ig => ig.id !== action.id);
+        default:
+            throw new Error('Should not get there');
+    }
+};
+
 function Ingredients() {
 
-    const [ingredients, setIngredients] = useState([]);
+    const [ingredients, dispatch] = useReducer(ingredientsReducer, []);
+    // const [ingredients, setIngredients] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState();
 
@@ -37,7 +51,8 @@ function Ingredients() {
 
 
     const filteredIngredient = useCallback((filteredIngredient) => {
-        setIngredients(filteredIngredient);
+        // setIngredients(filteredIngredient);
+        dispatch({type: 'SET', ingredients: filteredIngredient})
     }, []);
 
     const addIngredient = async (ingredient) => {
@@ -53,13 +68,14 @@ function Ingredients() {
         const responseData = await response.json();
         setIsLoading(false);
 
-        setIngredients(prevIngredients => [
-            ...prevIngredients,
-            {
-                id: responseData.name,
-                ...ingredient // contain title and amount
-            }]
-        );
+        // setIngredients(prevIngredients => [
+        //     ...prevIngredients,
+        //     {
+        //         id: responseData.name,
+        //         ...ingredient // contain title and amount
+        //     }]
+        // );
+        dispatch({type: 'ADD', ingredient: {id: responseData.name, ...ingredient}})
     };
 
     const removeIngredient = async (idToRemove) => {
@@ -71,9 +87,10 @@ function Ingredients() {
                 method: 'DELETE',
             });
             setIsLoading(false);
-            setIngredients(prevIngredients =>
-                prevIngredients.filter(ig => ig.id !== idToRemove)
-            )
+            // setIngredients(prevIngredients =>
+            //     prevIngredients.filter(ig => ig.id !== idToRemove)
+            // )
+            dispatch({type: 'DELETE', id: idToRemove});
         } catch (e) {
             // setIsLoading(false);
             setError(e.message);
